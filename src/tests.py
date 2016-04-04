c65b3475ca33db651fcec503c4caaba838de0a18
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-04-04 23:13:11 +0300

    Add tests.py, basic ideas

---
+++ src/tests.py
@@ -0,0 +1,23 @@
+# TODO: 
+# 1. Stemming/lemmatization of the file
+# 2. Find all words that start with capital later, when previous letter wasn't in '.!?' (sentance end)
+
+def get_names(text):
+	'''
+	text -> [(string, int)], (name, frequency) sorted by frequency
+	'''
+	raise Exception('Not implemented')
+
+
+def lemmatization(text):
+	'''
+	text -> text, make text lemmatization
+	'''
+	raise Exception('Not implemented')
+
+
+def clear(text):
+	'''
+	text -> text, remove all punctuation from the text
+	''' 
+	raise Exception('Not implemented')
