a08deb26108434786a6c2e8b1eb49d848ba0d5ec
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-04-07 22:18:40 +0300

    Move used functions to book_ops, update function descriptions, rename files

--- src/book_ops.py
+++ src/book_ops.py
@@ -1,23 +1,66 @@
+import string
+
+def get_names(text):
+	'''get (name, frequency) sorted by frequency from the text
+
+	text -> [(string, int)]
+	'''
+	is_capitalized = lambda word: len(word) and word[0].isupper()
+	words = filter(is_capitalized, text.split())
+	words_occurance = {}
+	for word in words:
+		if word in words_occurance:
+			words_occurance[word] += 1
+		else:
+			words_occurance[word] = 1
+	words_occurance = zip(words_occurance.keys(), words_occurance.values())
+	words_occurance = sorted(words_occurance, key=lambda item: item[1], reverse=True)
+	return words_occurance
+
+
+def get_text(path_to_book):
+	'''read book text from the file
+
+	string -> string
+	'''
+	with open(path_to_book, 'r') as book:
+		result = book.read()
+	return result
+
+
+def prepare_text(text):
+	'''prepare text for processing
+
+	string -> string
+	remove all puncutation
+	'''
+	exclude = set(string.punctuation)
+	return ''.join(ch for ch in text if ch not in exclude)
+
+
 
 def get_all_token_positions(text, token):
-	'''
-	text -> [int], all positions of token in the text
+	'''get all positions of the token in the text
+
+	text -> [int]
 	'''
 	words = text.split()
 	return [i for i, word in enumerate(words) if word == token]
 
 
 def count_token_occurrence(text, token):
-	'''
-	text, string -> int, find the number of occurances of the string in the text
+	'''find number of occurances of the token in the text
+
+	text, string -> int, 
 	'''
 	words = text.split()
 	return words.count(token)
 
 
 def get_text(path_to_book):
-	'''
-	string -> string, read book text from the book
+	'''read book text from the book
+
+	string -> string
 	'''
 	with open(path_to_book, 'r') as book:
 		result = book.read()
---
+++ src/experiments.py
@@ -0,0 +1,45 @@
+import book_ops
+
+# TODO: 
+# 1. Stemming/lemmatization of the file (for names may be unnecessary, becouse they are unchanged)
+# 2. Find all words that start with capital later, when previous letter wasn't in '.!?' (sentance end)
+# 3. Remove all non ascii characters from book
+# 4. Think about functions that accepts already splitted text, to avoid splitting in evety function
+#
+
+# Trying to get words that are not the beginning of the sentance
+def get_names_v2():
+	'''(name, frequency) sorted by frequency
+
+	text -> [(string, int)]
+	'''
+	for word in words:
+		if word in words_occurance:
+			words_occurance[word] += 1
+		else:
+			words_occurance[word] = 1
+	words_occurance = zip(words_occurance.keys(), words_occurance.values())
+	words_occurance = sorted(words_occurance, key=lambda item: item[1], reverse=True)
+	return words_occurance
+
+
+def lemmatization(text):
+	'''text lemmatization
+
+	text -> text
+	'''
+	raise Exception('Not implemented')
+
+
+if __name__ == '__main__':
+	prepared_book_path = 'test_result.txt'
+
+	raw_text = book_ops.get_text('../books/storm_of_swords.txt')
+	book_text = book_ops.prepare_text(raw_text)
+
+	with open(prepared_book_path, 'w+') as f:
+		f.write(book_text)
+	words_occurance = book_ops.get_names(book_text)
+	with open('get_names.txt', 'w+') as f:
+		for occurance in words_occurance:
+			f.write('{0} : {1}\n'.format(occurance[0], occurance[1]))
